c097bb15f240e978b7443d36347eb8f7f44550ce
Zifeng1997 <58804221+xzf89718@users.noreply.github.com>
2024-01-18 15:08:16 +0800

    Add file

--- config_git_user_email.sh
+++ config_git_user_email.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Modify your account 
-USEFUL_GIT_COMMAND_USERNAME=""
-USEFUL_GIT_COMMAND_USEREMAIL=""
+USEFUL_GIT_COMMAND_USERNAME=''
+USEFUL_GIT_COMMAND_USEREMAIL=''
 
 git config --global user.name $USEFUL_GIT_COMMAND_USERNAME
 git config --global user.email  $USEFUL_GIT_COMMAND_USEREMAIL
---
+++ submit_git_with_timetag.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+currentTime='date "+%Y-%m-%d %H:%M:%S"'
+SUBMIT_GIT_REVISION $currentTime
